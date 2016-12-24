d769d09beae5bf285174dcda0e2edee7ad06fc27
sumeet <sumeet@sumeet-PC>
2016-12-24 14:28:18 +0530

    Initial checkin newGitRepostory

---
+++ Test/src/com/light/apple/test/HelloGit.java
@@ -0,0 +1,8 @@
+package com.light.apple.test;
+
+public class HelloGit {
+	public void test()
+	{
+		System.out.println(" Hello Git test method");
+	}
+}
---
+++ Test/src/com/light/apple/test/TestingGit.java
@@ -0,0 +1,8 @@
+package com.light.apple.test;
+
+public class TestingGit {
+	public void test()
+	{
+		System.out.println(" hello test ");
+	}
+}
