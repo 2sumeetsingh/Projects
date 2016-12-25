bf49db9e3ee6dc94f853d179c493343404106cc4
sumeet <sumeet@sumeet-PC>
2016-12-25 21:24:24 +0530

    making it upper case

--- Test/src/com/light/apple/test/TestingGit.java
+++ Test/src/com/light/apple/test/TestingGit.java
@@ -3,7 +3,8 @@ package com.light.apple.test;
 public class TestingGit {
 	public void test()
 	{
-		System.out.println(" hello test ");
+		System.out.println(" HELOO TEST ");
+		System.out.println("#########################");
 		System.out.println(" hello test 1" );
 	}
 }
